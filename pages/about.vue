08e5416324f3470df8311af7192e5bc7ee52033d
joshua hurst <hurstindustries@gmail.com>
2021-02-02 13:21:59 -0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="bg-red-500">
+        ABOUT
+      </h1>
+      <div class="links">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        HOME
+      </h1>
+      <Test />
+      <div class="links">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+      </div>
+      <pre>{{ post }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  async asyncData({ params }) {
+    const response = await axios.get('https://api.nuxtjs.dev/posts');
+    const post = response.data;
+    return { post };
+  },
+};
+</script>
